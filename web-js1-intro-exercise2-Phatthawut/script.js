9facf43f62399b8ab029bd6ec72453eddc1cb4ad
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-06-10 10:56:32 +0700

    Delete web-js1-intro-exercise2-Phatthawut directory

--- web-js1-intro-exercise2-Phatthawut/script.js
+++
@@ -1,6 +0,0 @@
-function ChangeName(){
-    let changeText = prompt("type your word");
-    console.log("The button has been clicked");
-    document.getElementById("inputName").textContent = changeText;
-    return alert("Hello World");
-}
